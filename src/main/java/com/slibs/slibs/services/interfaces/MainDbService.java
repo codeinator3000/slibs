f056d1126021e5b525f485d64612a63b0be14cc2
codeinator3000 <danilabal1119@gmail.com>
2025-04-11 00:14:10 +0400

    add new method 'getAllLicenses' for MainDbService

--- src/main/java/com/slibs/slibs/services/interfaces/MainDbService.java
+++ src/main/java/com/slibs/slibs/services/interfaces/MainDbService.java
@@ -2,6 +2,7 @@ package com.slibs.slibs.services.interfaces;
 
 import java.util.List;
 
+import co.elastic.clients.elasticsearch.license.License;
 import co.elastic.clients.elasticsearch.snapshot.Repository;
 
 public interface MainDbService {
@@ -10,4 +11,9 @@ public interface MainDbService {
      * @return список репозиториев
      */
     public List<Repository> getAllRepositories();
+    /**
+     * Возвращает список всех лицензий, которые доступны в основной базе данных.
+     * @return список лицензий
+     */
+    public List<License> getAllLicenses();
 }
